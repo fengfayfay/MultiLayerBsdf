e556ef8a9518ddbf090232d3cfa33317e6b2a060
Feng Xie <feng.xie@gmail.com>
2018-02-07 19:14:07 -0800

    made uniform incide angle sampling the default

--- src/core/pbrt.h
+++ src/core/pbrt.h
@@ -173,7 +173,7 @@ struct Options {
     float alpha = 0.5;
     int numrays = 1e6;
     int dimension = 2;
-    bool uniformIncidentAngles = false;
+    bool uniformIncidentAngles = true;
 };
 
 extern Options PbrtOptions;
